fbacda5cffef7b7df0df0c7092e8bcb1f9807bfc
Andrew54757 <andrews54757@gmail.com>
2016-09-03 20:39:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,22 +7,32 @@ module.exports = class QuickMap {
     this.cleanin = 30;
   }
   clean() {
-    
+    this.objects = this.objects.filter(function(a) {return !a.deleted})
+    this.cleanin = 30
   }
-  set() {
-    
+  set(id,node) {
+    this.objects[id.toString()] = {node: node,deleted: false}
   }
-  delete() {
-    
+  delete(id) {
+    if (!this.objects[id.toString()] && !this.objects[id.toString()].deleted) return false;
+    this.objects[id.toString()] = {deleted: true}
+    this.cleanin --;
+    if (this.cleanin <= 0) this.clean();
+    return true
   }
   toObject() {
-    
+    var objects = {};
+    this.objects.forEach((ob,id)=>{
+      if (ob.deleted) return;
+      objects[id] = ob.node;
+    })
+    return objects;
   }
-  every() {
-    
+  every(a,b,c) {
+    return this.toObject().every(a,b,c)
   }
-  forEach() {
-    
+  forEach(a,b,c) {
+    return this.toObject().forEach(a,b,c)
   }
   
 }
