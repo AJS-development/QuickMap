92afa0a803527ec1335833932ec5d7ae203f1f61
Andrew54757 <andrews54757@gmail.com>
2016-09-04 13:45:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,49 +17,65 @@
 */ 
 module.exports = class QuickMap {
   constructor() {
-    this.objects = [];
+    this.objects = {};
     this.length = 0;
     this.cleanin = 30;
   }
   clean() {
-    var objects = [];
-    this.objects.forEach((ob,id)=>{
-      if (ob.deleted) return;
+    var objects = {};
+    for (var i in this.objects) {
+     var ob = this.objects[i];
+      if (ob.deleted) continue;
       objects[id] = ob;
-    })
+    }
     this.objects = objects;
     this.cleanin = 30
     this.length = this.objects.length;
   }
   set(id,node) {
-    this.length ++;
-    this.objects[id.toString()] = {node: node,deleted: false}
+     var key = "_" + id.toString();
+   if (!this.objects[key] || this.objects[key].deleted) this.length ++;
+    this.objects[key] = {node: node,deleted: false,key: id}
   }
   get(id) {
-    if (!this.objects[id.toString()] || this.objects[id.toString()].deleted) return false;
-    return this.objects[id.toString()].node
+     var object = this.objects["_" + id.toString()];
+    if (!object || object.deleted) return false;
+    return object.node
   }
   delete(id) {
-    this.length --;
-    if (!this.objects[id.toString()] && !this.objects[id.toString()].deleted) return false;
-    this.objects[id.toString()] = {deleted: true}
+    var object = this.objects["_" + id.toString()];
+    if (object && !object.deleted) this.length --;
+     
+    if (!object && !object.deleted) return false;
+    this.objects["_" + id.toString()] = {deleted: true}
     this.cleanin --;
     if (this.cleanin <= 0) this.clean();
     return true
   }
   toObject() {
     var objects = [];
-    this.objects.forEach((ob,id)=>{
-      if (ob.deleted) return;
-      objects[id] = ob.node;
-    })
+    for (var i in this.objects) {
+     var ob = this.objects[i];
+      if (ob.deleted) continue;
+      objects[ob.key] = ob.node;
+    }
     return objects;
   }
-  every(a,b,c) {
-    return this.toObject().every(a,b,c)
+  every(a) {
+     var visited = [];
+    for (var i in this.objects) {
+     if (this.objects[i].deleted) continue;
+     visited.push(i);
+     if (!a(this.objects[i].node,this.objects[i].key)) return;
+    }
   }
-  forEach(a,b,c) {
-    return this.toObject().forEach(a,b,c)
+  forEach(a) {
+     var visited = [];
+      for (var i in this.objects) {
+     if (this.objects[i].deleted) continue;
+     visited.push(i);
+     a(this.objects[i].node,this.objects[i].key)
+    }
   }
   
 }
