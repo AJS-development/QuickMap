ffb5f37a36ddfb672b673d7a2270cbfc162e1ae2
Andrew54757 <andrews54757@gmail.com>
2016-09-03 20:27:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+"use strict";
 
+module.exports = class QuickMap {
+  constructor() {
+    this.objects = {};
+    this.length = 0;
+    this.cleanin = 30;
+  }
+  clean() {
+    
+  }
+  set() {
+    
+  }
+  delete() {
+    
+  }
+  toObject() {
+    
+  }
+  every() {
+    
+  }
+  forEach() {
+    
+  }
+  
+}
